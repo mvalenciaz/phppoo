fdd7faf07dab8676b1a5a025451db90891a4eae1
mvalenciaz <mvalenciaz@misena.edu.co>
2024-03-08 20:53:53 -0500

    Prueba de Commit

--- class/Cuenta.php
+++ class/Cuenta.php
@@ -2,5 +2,19 @@
 
 class Cuenta 
 {
+    private $nombre;
+    private $nroCuenta;
+    private $tipoInteres;
+    private $saldo;
+
+    public function __construct($nombre, $nroCuenta, $tipoInteres, $saldo)
+    {
+        $this->nombre = $nombre;
+        $this->nroCuenta = $nroCuenta;
+        $this->tipoInteres = $tipoInteres;
+        $this->saldo = $saldo;
+    }
     
+    
+
 }
